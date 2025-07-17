0fc9d3016bc10723ce9c0ee546dbda9869f2b220
Amabile Vitória Lopes Ouriques <168000103+AmabileLopes@users.noreply.github.com>
2025-07-17 20:54:23 -0300

    Update Pessoa.cs

--- AcademiaDoZe.Domain/AcademiaDoZe.Domain/Pessoa.cs
+++ AcademiaDoZe.Domain/AcademiaDoZe.Domain/Pessoa.cs
@@ -15,15 +15,19 @@ namespace AcademiaDoZe.Domain
         String Cpf { get; set; }
         DateTime DataNascimento { get; set; }
         int Telefone { get; set; }
+        string Email {get; set; }
+        string Senha {get; set; }
         int NumeroCasa { get; set; }
         String Complemento {  get; set; }
 
-        public Pessoa( string nomecompleto, string cpf, DateTime datanascimento, int telefone, int numerocasa, string complemento, int cep, string pais, string estado, string cidade, string bairro, string nomelogradouro) : base(cep, pais, estado, cidade, bairro, nomelogradouro)
+        public Pessoa( string nomecompleto, string cpf, DateTime datanascimento, int telefone, string email, string senha, int numerocasa, string complemento, int cep, string pais, string estado, string cidade, string bairro, string nomelogradouro) : base(cep, pais, estado, cidade, bairro, nomelogradouro)
         {
             NomeCompleto = nomecompleto;
             Cpf = cpf;
             DataNascimento = datanascimento;
             Telefone = telefone;
+            Email = email;
+            Senha= senha;
             NumeroCasa = numerocasa;
             Complemento = complemento;
         }
