08d4dffbd374beb69ddd017d2d1060fcd57a8d11
Amabile Vitória Lopes Ouriques <168000103+AmabileLopes@users.noreply.github.com>
2025-07-17 20:42:46 -0300

    Adicionando e criando Classes Pessoa e Logradouro
    ajustar logradouro
    criar enums

---
+++ AcademiaDoZe.Domain/AcademiaDoZe.Domain/Logradouro.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademiaDoZe.Domain
+{
+    public class Logradouro
+    {
+        int Cep {  get; set; }
+        string Pais { get; set; }
+        string Estado {  get; set; }
+        string Cidade {  get; set; }
+        string Bairro { get; set; }
+        String Rua {  get; set; }
+
+        public Logradouro( int cep, string pais, string estado, string cidade, string bairro, string rua)
+        {
+            Cep = cep;
+            Pais = pais;
+            Estado = estado;
+            Cidade = cidade;
+            Bairro = bairro;
+            Rua = rua;
+        }
+    }
+}
---
+++ AcademiaDoZe.Domain/AcademiaDoZe.Domain/Pessoa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademiaDoZe.Domain
+{
+    public class Pessoa : Logradouro
+
+    {
+        string NomeCompleto { get; set; }
+        String Cpf { get; set; }
+        DateTime DataNascimento { get; set; }
+        int Telefone { get; set; }
+        int NumeroCasa { get; set; }
+        String Complemento {  get; set; }
+
+        public Pessoa( string nomecompleto, string cpf, DateTime datanascimento, int telefone, int numerocasa, string complemento, int cep, string pais, string estado, string cidade, string bairro, string rua) : base(cep, pais, estado, cidade, bairro, rua)
+        {
+            NomeCompleto = nomecompleto;
+            Cpf = cpf;
+            DataNascimento = datanascimento;
+            Telefone = telefone;
+            NumeroCasa = numerocasa;
+            Complemento = complemento;
+        }
+
+    }
+}
